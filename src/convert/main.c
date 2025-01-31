bbe9c63092b7b3067d0a498cb4cc2e5c696768e4
floral-hearts <hermit183@outlook.com>
2025-01-31 21:59:02 +0900

    Update main.c

--- src/convert/main.c
+++ src/convert/main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
+int main(int argc, char *argv[]) {
+    if(argc != 3) {
+        fputs("error: no input file path", stderr);
+        exit(0);
+    }
+    if(strcmp(argv[1], "-pdf") == 0)
+        execl("cnvt/-pdf.exe", "cnvt/-pdf.exe", argv[2], argv[3], NULL);
+    else if(strcmp(argv[1], "pdf-") == 0)
+        execl("cnvt/pdf-.exe", "cnvt/pdf-.exe", argv[2], argv[3], NULL);
+}
