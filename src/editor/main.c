8a35f05f7b5f4882b00a589899c166d6934f55e9
floral-hearts <hermit183@outlook.com>
2025-02-02 10:40:12 +0900

    Update main.c

--- src/editor/main.c
+++ src/editor/main.c
@@ -6,50 +6,51 @@
 
 DWORD escDownTime = 0;
 
+HWND HWInitialize(WNDPROC lpfnWndProc, HANDLE hInstance, LPCTSTR lpszClassName);
 LRESULT CALLBACK ExitProc(HWND hw, UINT msg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR psCmdLine, int nCmdShow) {
-    WNDCLASS wcStage = { 0 };
+    WNDCLASS wcStage;
     HWND wStage;
+	WNDCLASS wcExitButton;
+	HWND wExitButton;
     MSG msg;
 
-    wcStage.style = CS_HREDRAW | CS_VREDRAW;
-    wcStage.lpfnWndProc = ExitProc;
-    wcStage.hInstance = hInstance;
-    wcStage.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wcStage.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wcStage.hbrBackground = GetStockObject(NULL_BRUSH);
-    wcStage.lpszMenuName = NULL;
-    wcStage.lpszClassName = TEXT("wcStage");
-    if(!RegisterClass(&pffrWndInfo)) {
+	wcStage = HWInitialize(ExitProc, hInstance, "wcStage");
+    if(!RegisterClass(&wcStage)) {
         MessageBox(NULL, TEXT("create pffrWndInfo"), TEXT("ERROR"), MB_OK | MB_ICONERROR);
         return 1;
     }
-
     wStage = CreateWindow(
         TEXT("wcStage"),
         TEXT("Stage"),
-#if TYPE1
     	WS_POPUP,
-#elif TYPE2
-	    WS_OVERLAPPEDWINDOW & ~WS_CAPTION,
-#elif TYPE3
-    	(WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU) & ~WS_CAPTION,
-#else
-		WS_OVERLAPPED | WS_SYSMENU
-#endif
+//	    WS_OVERLAPPEDWINDOW & ~WS_CAPTION,
+        (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU) & ~WS_CAPTION,
+//        WS_OVERLAPPED | WS_SYSMENU,
         100, 100, 200, 200,
         NULL,
         NULL,
         hInstance,
         NULL
     );
-    if(!pffrWnd) {
+    if(!wStage) {
         MessageBox(NULL, TEXT("create pffrWndInfo"), TEXT("ERROR"), MB_OK | MB_ICONERROR);
         return 1;
     }
 
-    ShowWindow(pffrWnd, SW_SHOW);
+	wcExitButton = HWInitialize(ExitProc, hInstance, "wcExitButton");
+    if(!RegisterClass(&wcExitButton)) {
+        MessageBox(NULL, TEXT("create wcExitButton"), TEXT("ERROR"), MB_OK | MB_ICONERROR);
+        return 1;
+    }
+	wExitButton = CreateWindow(
+		TEXT("wcExitButton"),
+		TEXT("ExitButton"),
+		
+	)
+
+    ShowWindow(wStage, SW_SHOW);
 
     while(GetMessage(&msg, NULL, 0, 0)) {
 	    TranslateMessage(&msg);
@@ -59,7 +60,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR psCmdLine,
     return msg.wPara;
 }
 
-LRESULT CALLBACK WndProc(HWND hw, UINT msg, WPARAM wp, LPARAM lp) {
+HWND HWInitialize(WNDPROC lpfnWndProc, HANDLE hInstance, LPCTSTR lpszClassName) {
+	WNDCLASS wc;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = lpfnWndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = lpszClassName;
+	return wc;
+}
+
+LRESULT CALLBACK ExitProc(HWND hw, UINT msg, WPARAM wp, LPARAM lp) {
     if(msg == WM_DESTROY) {
         PostQuitMessage(0);
         return 0;
