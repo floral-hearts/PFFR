cd30a5ea62118be0bbe1cb2cfecb0df8f2e570ea
floral-hearts <hermit183@outlook.com>
2025-02-03 14:50:26 +0900

    Update main.c

--- src/editor/main.c
+++ src/editor/main.c
@@ -1,9 +1,7 @@
-#define TYPE1 0
-#define TYPE2 1
-#define TYPE3 0
-
 #include <windows.h>
 
+DWORD stageH = 400;
+DWORD stageV = 200;
 DWORD escDownTime = 0;
 
 HWND HWInitialize(WNDPROC lpfnWndProc, HANDLE hInstance, LPCTSTR lpszClassName);
@@ -12,11 +10,11 @@ LRESULT CALLBACK ExitProc(HWND hw, UINT msg, WPARAM wParam, LPARAM lParam);
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR psCmdLine, int nCmdShow) {
     WNDCLASS wcStage;
     HWND wStage;
-	WNDCLASS wcExitButton;
-	HWND wExitButton;
+    WNDCLASS wcExitButton;
+    HWND wExitButton;
     MSG msg;
 
-	wcStage = HWInitialize(ExitProc, hInstance, "wcStage");
+    wcStage = HWInitialize(ExitProc, hInstance, "wcStage");
     if(!RegisterClass(&wcStage)) {
         MessageBox(NULL, TEXT("create pffrWndInfo"), TEXT("ERROR"), MB_OK | MB_ICONERROR);
         return 1;
@@ -24,11 +22,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR psCmdLine,
     wStage = CreateWindow(
         TEXT("wcStage"),
         TEXT("Stage"),
-    	WS_POPUP,
-//	    WS_OVERLAPPEDWINDOW & ~WS_CAPTION,
+        WS_POPUP,
+//    WS_OVERLAPPEDWINDOW & ~WS_CAPTION,
         (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU) & ~WS_CAPTION,
 //        WS_OVERLAPPED | WS_SYSMENU,
-        100, 100, 200, 200,
+        CW_USEDEFAULT, CW_USEDEFAULT, stageH, stageV,
         NULL,
         NULL,
         hInstance,
@@ -39,21 +37,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR psCmdLine,
         return 1;
     }
 
-	wcExitButton = HWInitialize(ExitProc, hInstance, "wcExitButton");
+    wcExitButton = HWInitialize(ExitProc, hInstance, "wcExitButton");
     if(!RegisterClass(&wcExitButton)) {
         MessageBox(NULL, TEXT("create wcExitButton"), TEXT("ERROR"), MB_OK | MB_ICONERROR);
         return 1;
     }
-	wExitButton = CreateWindow(
-		TEXT("wcExitButton"),
-		TEXT("ExitButton"),
-		
-	)
+    wExitButton = CreateWindow(
+        TEXT("wcExitButton"),
+        TEXT("ExitButton"),
+        WS_CHILD | WS_VISIBLE | WS_BORDER,
+        stageH / 10 * 9, 0, stageH, stageV / 10,
+        NULL,
+        NULL,
+        hInstance,
+        NULL,
+    );
 
     ShowWindow(wStage, SW_SHOW);
 
     while(GetMessage(&msg, NULL, 0, 0)) {
-	    TranslateMessage(&msg);
+        TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
@@ -61,30 +64,30 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR psCmdLine,
 }
 
 HWND HWInitialize(WNDPROC lpfnWndProc, HANDLE hInstance, LPCTSTR lpszClassName) {
-	WNDCLASS wc;
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = lpfnWndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wc.lpszMenuName = NULL;
-	wc.lpszClassName = lpszClassName;
-	return wc;
+    WNDCLASS wc;
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = lpfnWndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = lpszClassName;
+    return wc;
 }
 
 LRESULT CALLBACK ExitProc(HWND hw, UINT msg, WPARAM wp, LPARAM lp) {
     if(msg == WM_DESTROY) {
         PostQuitMessage(0);
         return 0;
-	}
-	if(msg == WM_KEYDOWN) {
-		escDownTime = GetTickCount();
-	} else if(msg == WM_CHAR && wp == 0x1b && (GetTickCount() - escDownTime) >= 3) {
-		PostQuitMessage(0);
-		return 0;
-	}
+    }
+    if(msg == WM_KEYDOWN) {
+        escDownTime = GetTickCount();
+    } else if(msg == WM_CHAR && wp == 0x1b && (GetTickCount() - escDownTime) >= 3) {
+        PostQuitMessage(0);
+        return 0;
+    }
     return DefWindowProc(hw, msg, wp, lp);
 }
