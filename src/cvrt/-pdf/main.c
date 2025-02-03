5dad9a8130ccc23e3aa7611da712f6a8dc29ea2e
floral-hearts <hermit183@outlook.com>
2025-02-03 14:31:14 +0900

    Update main.c

--- src/cvrt/-pdf/main.c
+++ src/cvrt/-pdf/main.c
@@ -13,18 +13,28 @@ int main(int argc, char *argv[i]) {
     int pdfFile;
     if((pffrFile = open(argv[1], O_RDONLY)) != 0)
         return -1;
-    if((pdfFile = open(argv[2], O_RDONLY)) != 0)
+    if((pdfFile = open(argv[2], O_WRONLY)) != 0)
         return -1;
+
     for(int i = 0; i < 7; i ++) {
         char c;
         if(fread(pffrFile, &c, 1) == EOF)
-            error();0
+            error();
         if(c != "pffr\n\n\n"[i])
             error();
     }
     uint16_t cmd[4];
     cmd[3] = 0x0000;
+    uint16_t c;
     while(1) {
+        if(fread(pffrFile, &cmd[0], 1) == EOF)
+            error();
+        if(fread(pffrFile, &cmd[1], 1) == EOF)
+            error();
+        if(fread(pffrFile, &cmd[2], 1) == EOF)
+            error();
+        while(fread(pffrFile, &c, 1)) {
+        }
     }
 }
 
